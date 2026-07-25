c93f3e6717c580ab93cc3dc787ef3e43b41f3e32
桜井 諒 <ptolemaios923@gmail.com>
2026-07-25 21:42:47 +0900

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+const TARGET = {
+  capacity: "256gb",
+  model: "iphone15",
+};
+
+async function main() {
+  try {
+    const response = await fetch(
+      "https://www.apple.com/jp/shop/refurbished/iphone",
+    );
+    const html = await response.text();
+    const start = html.indexOf("window.REFURB_GRID_BOOTSTRAP");
+    if (start === -1) {
+      throw new Error("REFURB_GRID_BOOTSTRAP が見つかりませんでした");
+    }
+    const end = html.indexOf("};", start);
+
+    const bootstrap = html.slice(start, end + 1);
+
+    const jsonText = bootstrap.replace("window.REFURB_GRID_BOOTSTRAP = ", "");
+
+    const data = JSON.parse(jsonText);
+
+    console.log(data.tiles);
+
+    const items = data.tiles;
+    items.forEach((item: any) => {
+      // console.log(item);
+      // console.log(item.title);
+      console.log(item.filters.dimensions.refurbClearModel);
+      if (
+        item.filters.dimensions.dimensionCapacity === TARGET.capacity &&
+        item.filters.dimensions.refurbClearModel === TARGET.model
+      ) {
+        console.log("見つかった！");
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
