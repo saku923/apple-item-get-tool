a6e0a6b55047f62aeb6d0121b60006a757b453a1
桜井 諒 <ptolemaios923@gmail.com>
2026-07-26 20:34:42 +0900

    関数をmainから切り出し

---
+++ src/apple.ts
@@ -0,0 +1,50 @@
+const TARGET = {
+  capacity: "256gb",
+  model: "iphone15",
+};
+
+type Tile = {
+  filters: {
+    dimensions: {
+      dimensionCapacity: string;
+      refurbClearModel: string;
+      dimensionColor: string;
+    };
+  };
+};
+
+export const fetchHtml = async () => {
+  const response = await fetch(
+    "https://www.apple.com/jp/shop/refurbished/iphone",
+  );
+  return response.text();
+};
+
+export const extractBootstrap = (html: string) => {
+  const start = html.indexOf("window.REFURB_GRID_BOOTSTRAP");
+  if (start === -1) {
+    throw new Error("REFURB_GRID_BOOTSTRAP が見つかりませんでした");
+  }
+  const end = html.indexOf("};", start);
+
+  const bootstrap = html.slice(start, end + 1);
+
+  const jsonText = bootstrap.replace("window.REFURB_GRID_BOOTSTRAP = ", "");
+
+  const data = JSON.parse(jsonText);
+  return data;
+};
+
+export const findTargetItems = (items: Tile[]) => {
+  items.forEach((item) => {
+    console.log(item.filters.dimensions);
+    if (
+      item.filters.dimensions.dimensionCapacity === TARGET.capacity &&
+      item.filters.dimensions.refurbClearModel === TARGET.model
+    ) {
+      console.log("該当の商品が見つかりました！");
+      console.log(item.filters.dimensions.dimensionCapacity);
+      console.log(item.filters.dimensions.refurbClearModel);
+    }
+  });
+};
